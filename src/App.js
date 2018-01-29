2ae204f3bb01f13e7a10878309f08b711b0d80a5
lokeshh <lokeshhsharma@gmail.com>
2018-01-29 23:32:50 +0530

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -8,11 +9,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Yog Vasistha reader</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Row>
+            <Col>
+              <h3>Main content</h3>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
